27b700ee85d83fa234b20cd79cf834bd3b42fc62
vossr <pehkonenroni@gmail.com>
2024-08-24 10:23:37 +0300

    fix unicode input

--- rtx_api_july_2024.py
+++ rtx_api_july_2024.py
@@ -2,8 +2,9 @@ from http.client import HTTPConnection
 
 def send_message(message):
     connection = HTTPConnection('localhost', 8000)
-    headers = {'Content-type': 'text/plain'}
-    connection.request('POST', '/', message, headers)
+    headers = {'Content-type': 'text/plain; charset=utf-8'}
+    encoded_message = message.encode('utf-8')
+    connection.request('POST', '/', encoded_message, headers)
     response = connection.getresponse()
 
     if response.status == 200:
